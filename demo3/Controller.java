0359ed4e05d2089bb74af8eb5278903fde68ec9e
Golla Jyothika <125866072+Surefinish@users.noreply.github.com>
2024-02-26 16:30:57 +0530

    Add files via upload

---
+++ demo3/Controller.java
@@ -0,0 +1,15 @@
+import javafx.event.ActionEvent;
+public class Controller {
+    public void up(ActionEvent e){
+        System.out.println("UP");
+    }
+    public void up(ActionEvent e){
+        System.out.println("DOWN");
+    }
+    public void up(ActionEvent e){
+        System.out.println("LEFT");
+    }
+    public void up(ActionEvent e){
+        System.out.println("RIGHT");
+    }
+}
